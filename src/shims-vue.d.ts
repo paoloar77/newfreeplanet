6c3973b5addb0b28b84763ee0c29060a4bbc7f7e
paoloar77 <paolo.arena77@gmail.com>
2022-02-21 13:40:52 +0100

    Updated Some Libraries

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,7 +1,7 @@
 // Mocks all files ending in `.vue` showing them as plain Vue instances/* eslint-disable */
 /* eslint-disable */
 declare module '*.vue' {
-  import { defineComponent } from 'vue';
+  import type { defineComponent } from 'vue';
   const Component: ReturnType<typeof defineComponent>;
   export default Component;
 
